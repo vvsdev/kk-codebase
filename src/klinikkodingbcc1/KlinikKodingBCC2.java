181b4d05093d37a0bcf8425902ad33c014d90b50
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2020-11-05 14:48:15 +0700

    add pertemuan2 Challenge

---
+++ src/klinikkodingbcc1/KlinikKodingBCC2.java
@@ -0,0 +1,47 @@
+package klinikkodingbcc1;
+/**
+ *
+ * @author Ibrahimsyah
+ */
+public class KlinikKodingBCC2 {
+    
+    
+    /*
+    1. PREDIKSI CUACA
+    Baba adalah seorang peramal cuaca, dia bisa meramal cuaca hanya 
+    menggunakan beberapa data saja
+    Data tersebut adalah suhu udara sekitar dan kondisi awan. Cara baba 
+    memprediksi cuaca adalah sebagai berikut
+    1. Jika suhu udara saat itu dibawah 20 derajat, maka:
+        a. Jika awan TEBAL maka hari itu akan hujan
+        b. Jika awan TIDAK ADA maka hari itu akan cerah
+        c. Jika awan TIPIS maka hari itu hanya mendung
+    2. Jika suhu udara diatas 20 Derajat, maka hari itu akan cerah
+    */  
+
+    
+    /*
+    2. GAJI KARYAWAN   
+    Sebuah Perusahaan memiliki sistem penggajian karyawan berdasarkan jam 
+    kerja. Gaji pokok setiap karyawan adalah 10000 per hari.
+    Karyawan yang bekerja diatas 8jam sehari (Lembur) akan mendapat bonus
+    bonus tersebut diberikan sesuai dengan lamanya jam lemburnya dengan
+    ketentuan:
+       a. 1 Jam = Bonus 2000
+       b. 2 Jam = Bonus 4500
+       c. 3 Jam = Bonus 7000
+       d. diatas 3 Jam = Bonus 3000 x Jumlah Jam
+    Buatlah sistem penghitung gaji karyawan sesuai dengan ketentuan diatas
+    */
+    
+    /*
+    3. ANGKA ANEH
+    Terdapat sebuah angka N dan beberapa kondisi berikut ini
+        1. jika N ganjil maka cetak "Aneh"
+        2. jika N genap dan diantara 2 dan 5 maka cetak "Tidak Aneh"
+        3. jika N genap dan diantara 6 dan 20 maka cetak "Aneh"
+        4. jika N genap dan lebih dari 20 maka cetak "Tidak Aneh"
+    Buatlah program sederhana sesuai ketentuan diatas
+    */
+    
+}
